8a074e12d4980b5dee4c435994089d597a18fa46
moluopro <moluopro@qq.com>
2026-07-19 15:26:17 +0800

    test(example): cover cross-script typed arrays

--- example/cross_ref_a.gd
+++ example/cross_ref_a.gd
@@ -2,8 +2,14 @@ extends Node
 class_name CrossRefA
 
 var peer: CrossRefB
+var peers: Array[CrossRefB] = []
 
 
 func connect_peer(value: CrossRefB) -> CrossRefB:
     peer = value
     return peer
+
+
+func typed_peers(values: Array[CrossRefB]) -> Array[CrossRefB]:
+    peers = values
+    return peers
