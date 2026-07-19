4d7f6793dd41ecdc89da430d343a569b53036003
moluopro <moluopro@qq.com>
2026-07-19 15:26:17 +0800

    test(example): cover cross-script typed dictionaries

--- example/cross_ref_b.gd
+++ example/cross_ref_b.gd
@@ -2,8 +2,14 @@ extends Node
 class_name CrossRefB
 
 var peer: CrossRefA
+var peers_by_name: Dictionary[String, CrossRefA] = {}
 
 
 func connect_peer(value: CrossRefA) -> CrossRefA:
     peer = value
     return peer
+
+
+func typed_peers(values: Dictionary[String, CrossRefA]) -> Dictionary[String, CrossRefA]:
+    peers_by_name = values
+    return peers_by_name
