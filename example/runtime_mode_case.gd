c5e1c28d2103106b0ca71ad2ce00c291fc51cd4a
moluopro <moluopro@qq.com>
2026-07-20 15:06:20 +0800

    test(godot): expose runtime static contracts

--- example/runtime_mode_case.gd
+++ example/runtime_mode_case.gd
@@ -1,6 +1,7 @@
 extends RefCounted
 class_name RuntimeModeCase
 
+const RUNTIME_CONSTANT: int = 17
 static var static_initialized: bool = false
 static var instance_count: int = 0
 var initialized: bool = false
@@ -21,6 +22,10 @@ static func static_ready() -> bool:
 
 static func constructed_instances() -> int:
     return instance_count
+
+
+static func pure_static_answer() -> int:
+    return 42
 
 
 func instance_ready() -> bool:
